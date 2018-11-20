2f521b3568d39b06ead679fa56e263b54097f8af
root <root@localhost.localdomain>
2018-11-20 21:44:46 +0300

    SQLITE DATABASE

---
+++ app/src/main/java/pro/forecastanalytics/root/sqlite/MainActivity.java
@@ -0,0 +1,16 @@
+package pro.forecastanalytics.root.sqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    DatabaseHelper myDb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myDb =new DatabaseHelper(this);
+
+    }
+}
